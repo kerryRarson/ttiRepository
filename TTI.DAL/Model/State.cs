1443e265da4d3c1f7766b0cfc3a346b2f966587a
kelleyshomeemailaddr@gmail.com <kelleyshomeemailaddr@gmail.com>
2013-10-13 09:00:00 -0700

    initial commit

---
+++ TTI.DAL/Model/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TTI.DAL.Model
+{
+    public class State
+    {
+        public virtual int StateID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string FullName { get; set; }
+        public virtual string Code { get; set; }
+        public bool Active { get; set; }
+        public virtual DateTime LastUpdated { get; set; }
+        public virtual string LastUpdatedBy { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual string CreatedBy { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
