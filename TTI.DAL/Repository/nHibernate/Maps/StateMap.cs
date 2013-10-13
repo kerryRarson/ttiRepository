24ca654a68a3345382cff1ccb8aa70261ad662ad
kelleyshomeemailaddr@gmail.com <kelleyshomeemailaddr@gmail.com>
2013-10-13 09:21:48 -0700

    added StateMap

---
+++ TTI.DAL/Repository/nHibernate/Maps/StateMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Mapping;
+using TTI.DAL.Model;
+
+namespace TTI.DAL.Maps
+{
+    public class StateMap : ClassMap<State>
+    {
+        public StateMap()
+        {
+            Table("tState");
+            Id(x => x.StateID).Column("iState_ID");
+            Map(x => x.Name).Column("sName");
+            Map(x => x.FullName).Column("sFullName");
+            Map(x => x.Code).Column("sCode");
+            Map(x => x.Active).Column("bActive");
+            Map(x => x.LastUpdated).Column("dtLastUpdated");
+            Map(x => x.LastUpdatedBy).Column("sLastUpdatedBy");
+            Map(x => x.Created).Column("dtCreated");
+            Map(x => x.CreatedBy).Column("sCreatedBy");
+        }
+    }
+}
