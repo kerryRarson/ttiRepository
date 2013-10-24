c5002269674b4a5db6c7a9dbf07f058aad462430
kerryRarson <kelleyshomeemailaddr@gmail.com>
2013-10-24 16:33:10 -0600

    Update DemoPresenter.cs

--- TTI.DemoPresenter/DemoPresenter.cs
+++ TTI.DemoPresenter/DemoPresenter.cs
@@ -9,19 +9,24 @@ namespace TTI.Demo
     {
         protected readonly IDemoView _view;
         protected readonly DAL.Repository.IEntityRepository _entityRepository;
+        /*
+        * pass in the view on the construct & instansiate the db repository instance.
+        */
         public DemoPresenter(IDemoView view)
         {
             _view = view;
             _entityRepository = new DAL.Repository.NHibernate.EntityRepository();
         }
 
-        public void LoadStates(){
-
+        public void LoadStates()
+        {
             _view.UpdateStatus("in the presenter::Loading States...");
-
+            
+            // get the list of states from the repository
             var states = _entityRepository.GetStateList();
+            //pass the results back to the caller
             _view.BindStates(states);
-            _view.UpdateStatus(string.Format("presenter bound {0} states to the calling view.", states.Count));
+            //_view.UpdateStatus(string.Format("presenter bound {0} states to the calling view.", states.Count));
 
         }
     }
