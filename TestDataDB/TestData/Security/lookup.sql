4a0255604765b6760d3448090b7ff65d0c6473ac
kerryRarson <kelleyshomeemailaddr@gmail.com>
2014-03-06 10:20:15 -0700

    udpated to VS 2013

--- TestDataDB/TestData/Security/lookup.sql
+++ TestDataDB/TestData/Security/lookup.sql
@@ -1,4 +1,4 @@
 /* this is a test comment for demonstrating a 'git pull'*/
-﻿CREATE SCHEMA [lookup]
-    AUTHORIZATION [dbo];
-
+GO
+--﻿CREATE SCHEMA [lookup] AUTHORIZATION [dbo];
+GO
--- TestDataDB/TestData/lookup/Tables/EntityType.sql
+++ TestDataDB/TestData/lookup/Tables/EntityType.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [lookup].[EntityType] (
-    [EntityTypeID]   TINYINT      NOT NULL,
-    [EntityTypeDesc] VARCHAR (25) NOT NULL,
-    CONSTRAINT [PK_EntityType] PRIMARY KEY CLUSTERED ([EntityTypeID] ASC)
-);
+﻿--CREATE TABLE [lookup].[EntityType] (
+--    [EntityTypeID]   TINYINT      NOT NULL,
+--    [EntityTypeDesc] VARCHAR (25) NOT NULL,
+--    CONSTRAINT [PK_EntityType] PRIMARY KEY CLUSTERED ([EntityTypeID] ASC)
+--);
 
